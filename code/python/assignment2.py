3584fd2a1df3302bcb2d26674a1d17feb4cfdad3
Aayush Prabhu <102949498+AP-51@users.noreply.github.com>
2022-04-25 16:45:40 +0530

    Add files via upload

---
+++ code/python/assignment2.py
@@ -0,0 +1,14 @@
+import numpy as np
+# take a 3x3 matrix
+print("enter two 3by3 matrices")
+A = [list(map(int,input().split())) for i in range(3)]
+print("\n")
+B = [list(map(int,input().split())) for i in range(3)]
+result= [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+ 
+result = np.dot(A,B)
+print("product matrix is:") 
+for r in result:
+    print(r)
