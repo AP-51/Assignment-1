4b7e6480294efe857a51de154c87d0327cf0cf63
Aayush Prabhu <102949498+AP-51@users.noreply.github.com>
2022-05-09 17:34:36 +0530

    Add files via upload

---
+++ code/python/assignment3.py
@@ -0,0 +1,7 @@
+r=float(input("Enter radius of Circle "))
+l=float(input("Enter length of rectangle "))
+b=float(input("Enter breadth of rectangle "))
+f_area=3.14*r*r
+t_area=l*b
+p= f_area/t_area
+print("Probability of die falling in circle is: "+str(p))
